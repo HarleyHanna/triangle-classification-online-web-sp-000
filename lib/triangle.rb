0d7de0ce485c12608603be1218442a285f76e072
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-04 22:14:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,9 @@
 class Triangle
   
+  class TriangleError < StandardError
+    
+  end
+  
   attr_accessor :one, :two, :three
   
   def initialize(one, two, three)
@@ -9,5 +13,11 @@ class Triangle
   end
   
   def kind
+    if @one + @two < @three || @one + @three < @two || @two + @three < @one ||
+      TriangleError
+    elsif @one <= 0 || @two <= 0 || @three <= 0
+      TriangleError
+    else
+      
   
 end
